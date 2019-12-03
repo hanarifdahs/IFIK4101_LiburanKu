d92805eefb93922907e6877026b5d6a70623fe13
angelmetanosaa <54460286+angelmetanosaa@users.noreply.github.com>
2019-12-03 17:20:09 +0700

    Update and rename Sign up Customer.html to Register.php

---
+++ Register.php
@@ -0,0 +1,158 @@
+<?php require_once('Connections/koneksi.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
+  $insertSQL = sprintf("INSERT INTO login (email, username, nama, password) VALUES (%s, %s, %s, %s)",
+                       GetSQLValueString($_POST['email'], "text"),
+                       GetSQLValueString($_POST['username'], "text"),
+                       GetSQLValueString($_POST['nama'], "text"),
+                       GetSQLValueString($_POST['password'], "text"));
+
+  mysql_select_db($database_koneksi, $koneksi);
+  $Result1 = mysql_query($insertSQL, $koneksi) or die(mysql_error());
+
+  $insertGoTo = "http://localhost/liburanku/LiburanKu_Login.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
+    $insertGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $insertGoTo));
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: black;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+/* Add padding to containers */
+.container {
+  padding: 16px;
+  background-color: white;
+}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Overwrite default styles of hr */
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+/* Set a style for the submit button */
+.registerbtn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+.registerbtn:hover {
+  opacity: 1;
+}
+
+/* Add a blue text color to links */
+a {
+  color: dodgerblue;
+}
+
+/* Set a grey background color and center the text of the "sign in" section */
+.signin {
+  background-color: #f1f1f1;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+<form action="<?php echo $editFormAction; ?>" method="post" name="form1" id="form1">
+  <div class="container">
+    <h1>Register</h1>
+    <p>Please fill in this form to create an account.</p>
+    <hr>
+    <label for="email"><strong>Email</strong></label>
+    <input type="text" placeholder="Masukan email" name="email" required>
+   
+ <label for="username"><strong>Username</strong></label>
+    <input type="text" placeholder="Masukan Username" name="username" required>
+   
+ <label for="nama"><b>Nama</b></label>
+    <input type="text" placeholder="Masukan Nama" name="nama" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Masukan Password" name="password" required>
+    <hr>
+
+    <button type="submit" name="submit" class="registerbtn" >Register</button>
+  </div>
+  
+  <div class="container signin">
+    <p>Already have an account? <a href="http://localhost/liburanku/LiburanKu_Login.php#">Sign in</a>.</p>
+  </div>
+  <input type="hidden" name="MM_insert" value="form1" />
+</form>
+
+</body>
+</html>
